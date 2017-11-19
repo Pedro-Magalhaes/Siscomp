a8d6c9a3fe9a7ceff22a48cf438aabd03799b6ba
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2017-11-19 17:35:33 -0200

    Lab09 como enviado

--- Lab09/Lab02.c
+++ Lab09/Lab02.c
@@ -7,7 +7,7 @@
 #include <sys/msg.h>
 #include <sys/ipc.h>
 #define MAXFILA 8
-#define MAXPRODUZIDO 64
+#define MAXPRODUZIDO 12
 
 typedef struct mymsg {
     long int    mtype;     /* message type */
@@ -21,7 +21,7 @@ int main (void)
 	int pid_consumidor,pid_consumidor2,id_fila_msg,status;	
 	mymsg m;
 	struct msqid_ds buf;
-	m.mtype = 1; //sei la o que fazer com esse type
+	m.mtype = 1; 
 	id_fila_msg = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
 	
 	if(	msgctl(id_fila_msg,IPC_STAT,&buf) == -1)
@@ -38,35 +38,53 @@ int main (void)
 	
 	pid_consumidor=fork();	
 	if (pid_consumidor == 0) {			
-		//consumidor 1 
+		//consumidor 1 -- imprime impares
 		int i;					
 		for(i=0;i<MAXPRODUZIDO/2;i++)
 		{	
 			sleep(2);
-			status = msgrcv( id_fila_msg, &m, sizeof(m.meu_int), 0, 0); 
-			printf("CONSUMIDOR 1: Mensagem recebida %d \n ",m.meu_int);
-		}	
+			status = msgrcv( id_fila_msg, &m, sizeof(m.meu_int), 1, 0); 
+			if(status != -1){
+                    printf("CONSUMIDOR Impar: Mensagem recebida %d , status:%d \n",m.meu_int,status);
+                }else{
+                    printf("CONSUMIDOR Impar: ERRO AO LER MSG\n");
+                }
+        }	
+        exit(0);
 	
 	} 
 	else { //pai
 		
 		pid_consumidor2=fork();
-		if (pid_consumidor2 == 0) //consumidor 2
+		if (pid_consumidor2 == 0) //consumidor 2 - imprime pares
 		{
-			int i,status;				
+			int i,status;	
+            sleep(2); // pra deixar evidente que estão pegando msgs especificas
 			for(i=0;i<MAXPRODUZIDO/2;i++)
 			{	
 				sleep(2);
-				status = msgrcv( id_fila_msg, &m, sizeof(m.meu_int), 0, 0);
-										 
-				printf("CONSUMIDOR 2: Mensagem recebida %d \n ",m.meu_int);
-			}
+				status = msgrcv( id_fila_msg, &m, sizeof(m.meu_int), 2, 0);
+				if(status != -1){
+                    printf("CONSUMIDOR Par: Mensagem recebida %d , status:%d \n",m.meu_int,status);
+                }else{
+                    printf("CONSUMIDOR Par: ERRO AO LER MSG\n");
+                }			 
+				    
+            }
+             exit(0);
 		}
 		else
 		{ //pai produtor
 			int i;
 			for(i=0;i<MAXPRODUZIDO;i++)
 			{
+                if(i%2 == 0)
+                { //mensagem par é tipo 2
+                    m.mtype=2;
+                }
+                else{ //impar tipo 1
+                    m.mtype=1;
+                }
 				m.meu_int = i;
 				status = msgsnd( id_fila_msg, &m, sizeof(m.meu_int), 0);
 				printf("\n\nPRODUTOR: enviado %d, status: %d\n\n",m.meu_int,status);
@@ -79,6 +97,6 @@ int main (void)
 		
 	}
 	waitpid(-1,&status,0);
-	waitpid(-1,&status,0);
+    waitpid(-1,&status,0);    
 	return 0;
 }
